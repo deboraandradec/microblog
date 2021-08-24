1085eacd1230d0bdfdb6a2bd40a6cbf60c27a77c
Débora Cavalcante <deboradeandrade13@gmail.com>
2021-08-24 20:58:26 -0300

    alterações

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    usuario = {'nome': 'Débora', 'sobrenome': 'Cavalcante'}
+    return render_template('index.html', title='Página Inicial', usuario=usuario)
+@app.route('/contato')
+def contato():
+	return render_template('contato.html', title='Entre em Contato')
